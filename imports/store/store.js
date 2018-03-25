803b8bc8c22ac473b084a5955a003ad7a3471589
Jacob Sussman <20190324@student.nths.net>
2018-03-25 11:35:11 -0500

    fix
    fixed logger

--- imports/store/store.js
+++ imports/store/store.js
@@ -1,15 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import logger from 'redux-logger'
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import reducer from '../reducers/index';
 
-const middleware = [thunk, logger]
-const enhancers = compose(
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-);
+const middleware = [thunk, logger];
+const enhancers = window.devToolsExtension
+    ? window.devToolsExtension()
+    : f => f;
 
-const store = createStore(reducer, applyMiddleware(...middleware), enhancers);
+const store = createStore(
+    reducer,
+    compose(applyMiddleware(...middleware), enhancers)
+);
 
 console.log(store.getState());
 
