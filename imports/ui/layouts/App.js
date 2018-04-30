5042520bf6d3a7db2147c697e7dde408ff7805ef
Jacob Sussman <jacobsussman5@gmail.com>
2018-04-29 21:26:03 -0500

    -Implemented sticky footer -Minor styling tweaks

--- imports/ui/layouts/App.js
+++ imports/ui/layouts/App.js
@@ -1,62 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import { Layout } from 'antd';
-const { Header, Content, Footer } = Layout;
+import { Layout } from 'antd'
+const { Header, Content, Footer } = Layout
 
-import { Router, Route, Switch, Link } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
-import { syncHistoryWithStore } from 'react-router-redux';
+import { Router, Route, Switch, Link } from 'react-router-dom'
+import createBrowserHistory from 'history/createBrowserHistory'
+import { syncHistoryWithStore } from 'react-router-redux'
 
-import Home from '../pages/Home.js';
-import Login from '../pages/Login.js';
-import SignUp from '../pages/SignUp.js';
-import API from '../pages/API.js';
-import Settings from '../pages/Settings.js';
-import Profile from '../pages/Profile.js';
-import Data from '../pages/Data.js';
-import NotFound from '../pages/NotFound.js';
+import Home from '../pages/Home.js'
+import Login from '../pages/Login.js'
+import SignUp from '../pages/SignUp.js'
+import API from '../pages/API.js'
+import Settings from '../pages/Settings.js'
+import Profile from '../pages/Profile.js'
+import Data from '../pages/Data.js'
+import NotFound from '../pages/NotFound.js'
 
-import NavBar from '../components/NavBar.js';
-import store from '../../store/store';
+import NavBar from '../components/NavBar.js'
+import store from '../../store/store'
 
-const browserHistory = createBrowserHistory();
+const browserHistory = createBrowserHistory()
 
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(browserHistory, store)
 
 class App extends Component {
 	render() {
 		return (
-			<Layout>
+			<Layout
+				style={{
+					display: 'flex',
+					minHeight: '100vh',
+					flexDirection: 'column'
+				}}
+			>
 				<Router history={browserHistory}>
-					<div>
+					<div style={{ flex: 1 }}>
 						<NavBar />
 						<Content>
 							<Switch>
-								<Route
-									exact
-									path="/"
-									component={Home}
-								/>
-								<Route
-									exact
-									path="/login"
-									component={Login}
-								/>
+								<Route exact path="/" component={Home} />
+								<Route exact path="/login" component={Login} />
 								<Route
 									exact
 									path="/signup"
 									component={SignUp}
 								/>
-								<Route
-									exact
-									path="/data"
-									component={Data}
-								/>
-								<Route
-									exact
-									path="/api"
-									component={API}
-								/>
+								<Route exact path="/data" component={Data} />
+								<Route exact path="/api" component={API} />
 								<Route
 									exact
 									path="/settings"
@@ -77,8 +67,8 @@ class App extends Component {
 					Privacy Policy
 				</Footer>
 			</Layout>
-		);
+		)
 	}
 }
 
-export default App;
+export default App
