73ce167cff9524210da5bafbee8e3e0ede343b68
Connor Larkin <connor.larkin1@gmail.com>
2018-05-20 02:49:03 -0500

    oops i changed all file permissions
    client => google bucket does work doe

--- imports/api/userData/methods.js
+++ imports/api/userData/methods.js
@@ -1,6 +1,9 @@
 import { check } from 'meteor/check';
 import { Accounts } from 'meteor/accounts-base'
 
+var gcloud, gcs, bucket, bucketMetadata, Request, bound, Collections = {};
+var fs = Npm.require('fs');
+
 Meteor.methods({
     'UserData.insert' (data) { //data param is an object to add to user profile
       if (!Meteor.userId()) {
@@ -24,4 +27,56 @@ Meteor.methods({
         Accounts.removeEmail(Meteor.userId(), Meteor.user().emails[0].address)
         Accounts.addEmail(Meteor.userId(), newEmail);
       },
+      'file-upload': function (data) {
+        console.log("received file " + data.name );
+        console.log(__dirname)
+        //fs.writeFile("temp/test.png",data.name, data);
+        fs.writeFile('./myfile', data.blob, { flag: 'w' }, function(err) {
+          if (err) 
+              return console.error(err); 
+          fs.readFile('./myfile.txt', 'utf-8', function (err, data) {
+          if (err)
+              return console.error(err);
+          console.log(data);
+
+          uploadFile('alphalearn', './myfile')
+          });
+      })
+
+        fs.writeFile("temp.jpeg" ,data.blob, function(err) {
+          if(err) {
+            console.log("err", err);
+          } else {
+            console.log("yay" );
+          }
+        }) 
+        
+     }
 });
+
+function uploadFile(bucketName, filename) {
+  // [START storage_upload_file]
+  // Imports the Google Cloud client library
+  const Storage = require('@google-cloud/storage');
+
+  // Creates a client
+  const storage = new Storage();
+
+  /**
+   * TODO(developer): Uncomment the following lines before running the sample.
+   */
+  // const bucketName = 'Name of a bucket, e.g. my-bucket';
+  // const filename = 'Local file to upload, e.g. ./local/path/to/file.txt';
+
+  // Uploads a local file to the bucket
+  storage
+    .bucket(bucketName)
+    .upload(filename)
+    .then(() => {
+      console.log(`${filename} uploaded to ${bucketName}.`);
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+  // [END storage_upload_file]
+}
--- imports/ui/pages/Home.js
+++ imports/ui/pages/Home.js
@@ -16,6 +16,24 @@ import { connect } from 'react-redux';
 const { Header, Footer, Sider, Content } = Layout;
 
 class Home extends Component {
+    getFile = (e) => {
+        e.preventDefault();
+        let reader = new FileReader();
+        let file = e.target.files[0];
+        reader.onloadend = (theFile) => {
+            var data = {
+                blob: theFile.target.result, name: file.name,
+                //visitorId:  this.props.socketio.visitorId
+            };
+            //console.log(data.blob);
+            Meteor.call('file-upload', data);
+            //this.props.socketio.emit('file-upload', data);
+        };
+        console.log(file)
+        reader.readAsDataURL(file);
+        
+    }
+    
     render() {
         const { getFieldDecorator } = this.props.form;
         const { username } = this.props;
@@ -27,6 +45,10 @@ class Home extends Component {
                         <Row style={{ marginTop: '5%' }}>
                             <Col span={7} offset={8}>
                                 <h1>{username}</h1>
+                                <input onChange={this.getFile} id="fileUpload" type="file" className="upload"/>
+
+
+
                             </Col>
                         </Row>
                     </Content>
